cc67b4bffa8e5024c75c460254b0d6c654a92f22
luiz silva <48223061-l8758711@users.noreply.replit.com>
2026-03-07 20:56:45 +0000

    .

--- app/extensions.py
+++ app/extensions.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_openapi3 import OpenAPI
+from flask_openapi3.openapi import OpenAPI
 
 db = SQLAlchemy()
 migrate = Migrate()
